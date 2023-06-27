38b968a642078c734aaf9588a21490ff0fe14791
baritaha <135865393+baritaha@users.noreply.github.com>
2023-06-27 16:20:16 +0300

    Update check.php

--- check.php
+++ check.php
@@ -1,25 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Login Page</title>
-  <link rel="stylesheet" type="text/css" href="styles.css">
-</head>
-<body>
-  <div class="container">
-    <form action="check.php" method="POST">
-      <h2 style=color:red>Login</h2>
-      <label for="username"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="username" required>
-<br>
-      <label for="password"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="password" required>
-
-      <button type="submit" class="btn">Login</button>
-    </form>
-  </div>
-</body>
-</html>
-
 <?php
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if the submitted username and password match the desired credentials
